6a390a71b2f0200d6e9d445fb4ee8d85f381bea3
Jared Bajkowski <bajkowski333@live.missouristate.edu>
2017-03-20 18:58:31 -0500

    Add files via upload

---
+++ folio/js/script.js
@@ -0,0 +1,12 @@
+/* SCROLL REVEAL JS */
+
+var fooReveal = {
+  origin : 'left',
+  delay    : 100,
+  distance : '500px',
+  easing   : 'ease-in-out',
+
+};
+
+window.sr = ScrollReveal();
+sr.reveal('#side-nav', fooReveal);
