c130d12476ebfd82c7773847dee26aa8f6971dac
jaredbaj <jaredbaj@gmail.com>
2017-04-03 22:33:15 -0500

    whitespace

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
-/* SCROLL REVEAL JS */
+// SCROLL REVEAL JS 
 
 var fooReveal = {
-  origin : 'left',
+  origin : 'right',
   delay    : 100,
-  distance : '500px',
+  distance : '100px',
   easing   : 'ease-in-out',
 
 };
 
 window.sr = ScrollReveal();
-sr.reveal('#side-nav', fooReveal);
+sr.reveal('.fuck', fooReveal);
+
+
+
+// SMOOTH SCROLL 
+
+
+
+
+$(function(){
+    $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+      var windowWidth = $(window).width();
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+          window.location.hash = target;
+      });
+      
+  });
+});
+
+
+
+
+
